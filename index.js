f2f46f9f47621566b595ffd29cc3ee6fd48ea189
Chris Car <chris@metaversejs.org>
2016-06-02 18:39:27 -0400

    some progress

--- index.js
+++ index.js
@@ -19,12 +19,20 @@ AFRAME.registerComponent('particle-system', {
         }, 
         maxAge: {
             type: 'number',
-            default: 5
+            default: 2
         },
         type: {
             type: 'number',
             default: SPE.distributions.BOX
             /* SPE.distributions.SPHERE, SPE.distributions.DISC */
+        },
+        rotation_axis: {
+            type: 'string',
+            default: 'x'
+        },
+        rotation_angle: {
+            type: 'number',
+            default: 0
         },
         accelerationValue: {
             type: 'vec3',
@@ -73,7 +81,7 @@ AFRAME.registerComponent('particle-system', {
         },
         particleCount: {
             type: 'number',
-            default: 100
+            default: 2000
         }, 
         maxParticleCount: {
             type: 'number',
@@ -102,12 +110,21 @@ AFRAME.registerComponent('particle-system', {
         );
 
         this.clock = new THREE.Clock();
-      
+
         this.particleGroup = new SPE.Group({
             texture: {
                     value: particle_texture 
-                }
+                },
+            maxParticleCount: this.data.maxParticleCount
           });
+
+
+          /* color */
+          var color_arr = [];
+          this.data.color.split(',').forEach((function(c) {
+              color_arr.push(new THREE.Color(this.hexToRgb(c).r, this.hexToRgb(c).g, this.hexToRgb(c).b));
+          }).bind(this));
+
 
           var emitter = new SPE.Emitter({
               maxAge: {
@@ -117,23 +134,23 @@ AFRAME.registerComponent('particle-system', {
                   value: this.data.type
               },
               position: {
-                  value: this.el.object3D.position,
+                  value: this.el.object3D.position, 
                   spread: new THREE.Vector3(0, 0, 0)
               },
               rotation: {
-                  axis: this.el.object3D.rotation,
-                  angle: 0
+                  axis: (this.data.rotation_axis=='x'?new THREE.Vector3(1, 0, 0):(this.data.rotation_axis=='y'?new THREE.Vector3(0, 1, 0):(this.data.rotation_axis=='z'?new THREE.Vector3(0, 0, 1):new THREE.Vector3(0, 1, 0)))), 
+                  angle: this.data.rotation_angle
               },
               acceleration: {
-                  value: this.data.accelerationValue,
-                  spread: this.data.accelerationSpread
+                  value: new THREE.Vector3(this.data.accelerationValue.x, this.data.accelerationValue.y, this.data.accelerationValue.z),
+                  spread: new THREE.Vector3(this.data.accelerationSpread.x, this.data.accelerationSpread.y, this.data.accelerationSpread.z)
               },
               velocity: {
-                  value: this.data.velocityValue,
-                  spread: this.data.velocitySpread 
+                  value: new THREE.Vector3(this.data.velocityValue.x, this.data.velocityValue.y, this.data.velocityValue.z), 
+                  spread: new THREE.Vector3(this.data.velocitySpread.x, this.data.velocitySpread.y, this.data.velocitySpread.z)  
               },
               color: {
-                  value: [ new THREE.Color('white'), new THREE.Color('red') ]
+                  value: color_arr 
               },
               size: {
                   value: this.data.size
@@ -151,8 +168,7 @@ AFRAME.registerComponent('particle-system', {
                   value: this.data.duration
               },
               opacity: this.data.opacity,
-              particleCount: this.data.particleCount,
-              maxParticleCount: this.data.maxParticleCount
+              particleCount: this.data.particleCount
           });
 
           this.particleGroup.addEmitter(emitter);
@@ -166,7 +182,19 @@ AFRAME.registerComponent('particle-system', {
     },
 
 
-    remove: function() {}
+    remove: function() {},
+
+
+    hexToRgb: function(hex) {
+
+        var result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex);
+        return result ? {
+            r: parseInt(result[1], 16),
+            g: parseInt(result[2], 16),
+            b: parseInt(result[3], 16)
+        } : null;
+
+    }
 
 });
 
