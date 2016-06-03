fabeb5f0b8aff217df6c90829189b87bc54dc2cc
Chris Car <chris@metaversejs.org>
2016-06-02 23:22:31 -0400

    schema impl.

--- index.js
+++ index.js
@@ -15,32 +15,35 @@ AFRAME.registerComponent('particle-system', {
     schema: {
         preset: {
             type: 'string',
-            default: 'dust'
         }, 
         maxAge: {
             type: 'number',
             default: 2
+        },
+        positionSpread: {
+            type: 'vec3',
+            default: {x: 0, y: 0, z: 0}
         },
         type: {
             type: 'number',
             default: SPE.distributions.BOX
             /* SPE.distributions.SPHERE, SPE.distributions.DISC */
         },
-        rotation_axis: {
+        rotationAxis: {
             type: 'string',
             default: 'x'
         },
-        rotation_angle: {
+        rotationAngle: {
             type: 'number',
-            default: 0
+            default: 3.14 
         },
         accelerationValue: {
             type: 'vec3',
-            default: {x: 0, y: -10, z: 0}
+            default: {x: 0, y: 0, z: 0}
         },
         accelerationSpread: {
             type: 'vec3',
-            default: {x: 10, y: 0, z: 10}
+            default: {x: 0, y: 0, z: 0}
         },
         velocityValue: {
             type: 'vec3',
@@ -86,18 +89,33 @@ AFRAME.registerComponent('particle-system', {
         maxParticleCount: {
             type: 'number',
             default: 250000
+        },
+        texture: {
+            type: 'string',
+            default: './images/smokeparticle.png'
         } 
     },
 
 
-    init: function() {},
+    init: function() {
+
+        /*this.presets['snow'] = {
+            rotationAxis: 'x', 
+            rotationAngle: 3.14, 
+            accelerationSpread: {x: 0, y: 0, z: 0}, 
+            accelerationValue: {x: 0, y: 0, z: 0},
+            positionSpread: {x: 100, y: 100, z: 100},
+            texture: this.data.texture,
+        };*/
+
+    },
  
 
     update: function (oldData) {
 
         var loader = new THREE.TextureLoader();
         var particle_texture = loader.load(
-            './images/smokeparticle.png',
+            this.data.texture,
             function (texture) {
                 return texture;
             },
@@ -113,66 +131,69 @@ AFRAME.registerComponent('particle-system', {
 
         this.particleGroup = new SPE.Group({
             texture: {
-                    value: particle_texture 
-                },
+                value: particle_texture 
+            },
             maxParticleCount: this.data.maxParticleCount
-          });
+        });
 
 
-          /* color */
-          var color_arr = [];
-          this.data.color.split(',').forEach((function(c) {
-              color_arr.push(new THREE.Color(this.hexToRgb(c).r, this.hexToRgb(c).g, this.hexToRgb(c).b));
-          }).bind(this));
+        /* color */
+        var color_arr = [];
+        this.data.color.split(',').forEach((function(c) {
+            color_arr.push(new THREE.Color(this.hexToRgb(c).r, this.hexToRgb(c).g, this.hexToRgb(c).b));
+        }).bind(this));
 
 
-          var emitter = new SPE.Emitter({
-              maxAge: {
-                  value: this.data.maxAge
-              },
-              type: {
-                  value: this.data.type
-              },
-              position: {
-                  value: this.el.object3D.position, 
-                  spread: new THREE.Vector3(0, 0, 0)
-              },
-              rotation: {
-                  axis: (this.data.rotation_axis=='x'?new THREE.Vector3(1, 0, 0):(this.data.rotation_axis=='y'?new THREE.Vector3(0, 1, 0):(this.data.rotation_axis=='z'?new THREE.Vector3(0, 0, 1):new THREE.Vector3(0, 1, 0)))), 
-                  angle: this.data.rotation_angle
-              },
-              acceleration: {
-                  value: new THREE.Vector3(this.data.accelerationValue.x, this.data.accelerationValue.y, this.data.accelerationValue.z),
-                  spread: new THREE.Vector3(this.data.accelerationSpread.x, this.data.accelerationSpread.y, this.data.accelerationSpread.z)
-              },
-              velocity: {
-                  value: new THREE.Vector3(this.data.velocityValue.x, this.data.velocityValue.y, this.data.velocityValue.z), 
-                  spread: new THREE.Vector3(this.data.velocitySpread.x, this.data.velocitySpread.y, this.data.velocitySpread.z)  
-              },
-              color: {
-                  value: color_arr 
-              },
-              size: {
-                  value: this.data.size
-              },
-              wiggle: {
-                  value: this.data.wiggle
-              },
-              drag: {
-                  value: this.data.drag
-              },
-              direction: {
-                  value: this.data.direction
-              },
-              duration: {
-                  value: this.data.duration
-              },
-              opacity: this.data.opacity,
-              particleCount: this.data.particleCount
-          });
+        var emitter = new SPE.Emitter({
+            maxAge: {
+                value: this.data.maxAge
+            },
+            type: {
+                value: this.data.type
+            },
+            position: {
+                value: this.el.object3D.position, 
+                spread: new THREE.Vector3(this.data.positionSpread.x, this.data.positionSpread.y, this.data.positionSpread.z)
+                //spreadClamp: new THREE.Vector3( 2, 2, 2 ),
+                //radius: 4
+            },
+            rotation: {
+                axis: (this.data.rotationAxis=='x'?new THREE.Vector3(1, 0, 0):(this.data.rotationAxis=='y'?new THREE.Vector3(0, 1, 0):(this.data.rotationAxis=='z'?new THREE.Vector3(0, 0, 1):new THREE.Vector3(0, 1, 0)))), 
+                angle: this.data.rotationAngle,
+                static: true
+            },
+            acceleration: {
+                value: new THREE.Vector3(this.data.accelerationValue.x, this.data.accelerationValue.y, this.data.accelerationValue.z),
+                spread: new THREE.Vector3(this.data.accelerationSpread.x, this.data.accelerationSpread.y, this.data.accelerationSpread.z)
+            },
+            velocity: {
+                value: new THREE.Vector3(this.data.velocityValue.x, this.data.velocityValue.y, this.data.velocityValue.z), 
+                spread: new THREE.Vector3(this.data.velocitySpread.x, this.data.velocitySpread.y, this.data.velocitySpread.z)  
+            },
+            color: {
+                value: color_arr 
+            },
+            size: {
+                value: this.data.size
+            },
+            wiggle: {
+                value: this.data.wiggle
+            },
+            drag: {
+                value: this.data.drag
+            },
+            direction: {
+                value: this.data.direction
+            },
+            duration: {
+                value: this.data.duration
+            },
+            opacity: this.data.opacity,
+            particleCount: this.data.particleCount
+        });
 
-          this.particleGroup.addEmitter(emitter);
-          this.el.sceneEl.object3D.add(this.particleGroup.mesh);
+        this.particleGroup.addEmitter(emitter);
+        this.el.sceneEl.object3D.add(this.particleGroup.mesh);
     },
 
 
