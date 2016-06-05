46bebe7a84ae423a623c27db176f8c741baa111d
Chris Car <chris@metaversejs.org>
2016-06-05 14:59:12 -0400

    improved snow preset

--- dist/aframe-particle-system-component.js
+++ dist/aframe-particle-system-component.js
@@ -90,7 +90,6 @@
 	            type: 'vec3'
 	        },
 	        wiggle: {
-	            type: 'number'
 	        },
 	        drag: {
 	            type: 'number'
@@ -142,7 +141,7 @@
 	            accelerationSpread: (this.data.accelerationSpread.x!==0&&this.data.accelerationSpread.y!==0&&this.data.accelerationSpread.z!==0?this.data.accelerationSpread:{x: 10, y: 0, z: 10}), 
 	            velocityValue: (this.data.velocityValue.x!==0&&this.data.velocityValue.y!==0&&this.data.velocityValue.z!==0?this.data.velocityValue:{x: 0, y: 25, z: 0}),
 	            velocitySpread: (this.data.velocitySpread.x!==0&&this.data.velocitySpread.y!==0&&this.data.velocitySpread.z!==0?this.data.velocitySpread:{x: 10, y: 7.5, z: 10}), 
-	            wiggle: (this.data.wiggle!==0?this.data.wiggle:0),
+	            wiggle: (this.data.wiggle.value!==0&&this.data.wiggle.spread!==0?this.data.wiggle:{ value: 0, spread: 0}),
 	            drag: (this.data.drag!==0?this.data.drag:0),
 	            color: (this.data.color!==''?this.data.color:'#FFFFFF,#0000FF,#FF0000'),
 	            size: (this.data.size!==0?this.data.size:1),
@@ -165,7 +164,7 @@
 	            accelerationSpread: (this.data.accelerationSpread.x!==0&&this.data.accelerationSpread.y!==0&&this.data.accelerationSpread.z!==0?this.data.accelerationSpread:{x: 0, y: 0, z: 0}), 
 	            velocityValue: (this.data.velocityValue.x!==0&&this.data.velocityValue.y!==0&&this.data.velocityValue.z!==0?this.data.velocityValue:{x: 1, y: 0.3, z: 1}),
 	            velocitySpread: (this.data.velocitySpread.x!==0&&this.data.velocitySpread.y!==0&&this.data.velocitySpread.z!==0?this.data.velocitySpread:{x: 0.5, y: 1, z: 0.5}), 
-	            wiggle: (this.data.wiggle!==0?this.data.wiggle:0),
+	            wiggle: (this.data.wiggle.value!==0&&this.data.wiggle.spread!==0?this.data.wiggle:{ value: 0, spread: 0}),
 	            drag: (this.data.drag!==0?this.data.drag:0),
 	            color: (this.data.color!==''?this.data.color:'#FFFFFF'),
 	            size: (this.data.size!==0?this.data.size:1),
@@ -173,6 +172,29 @@
 	            direction: (this.data.direction!==0?this.data.direction:1),
 	            duration: (this.data.duration!=null?this.data.duration:null),
 	            particleCount: (this.data.particleCount!==0?this.data.particleCount:100),
+	            texture: (this.data.texture!==''?this.data.texture:'./images/smokeparticle.png'),
+	            randomize: false
+	        };
+
+
+	        this.presets['snow'] = {
+	            maxAge: (this.data.maxAge!==0?this.data.maxAge:20),
+	            positionSpread: (this.data.positionSpread.x!==0&&this.data.positionSpread.y!==0&&this.data.positionSpread.z!==0?this.data.positionSpread:{x:100,y:100,z:100}),
+	            type: (this.data.type!==0?this.data.type:SPE.distributions.BOX), /* SPE.distributions.SPHERE, SPE.distributions.DISC */
+	            rotationAxis: (this.data.rotationAxis!==''?this.data.rotationAxis:'x'), 
+	            rotationAngle: (this.data.rotationAngle!==0?this.data.rotationAngle:3.14), 
+	            accelerationValue: (this.data.accelerationValue.x!==0&&this.data.accelerationValue.y!==0&&this.data.accelerationValue.z!==0?this.data.accelerationValue:{x: 0, y: 0, z: 0}),
+	            accelerationSpread: (this.data.accelerationSpread.x!==0&&this.data.accelerationSpread.y!==0&&this.data.accelerationSpread.z!==0?this.data.accelerationSpread:{x: 0.2, y: 0, z: 0.2}), 
+	            velocityValue: (this.data.velocityValue.x!==0&&this.data.velocityValue.y!==0&&this.data.velocityValue.z!==0?this.data.velocityValue:{x: 0, y: 8, z: 0}),
+	            velocitySpread: (this.data.velocitySpread.x!==0&&this.data.velocitySpread.y!==0&&this.data.velocitySpread.z!==0?this.data.velocitySpread:{x: 2, y: 0, z: 2}), 
+	            wiggle: (this.data.wiggle.value!==0&&this.data.wiggle.spread!==0?this.data.wiggle:{ value: 0, spread: 0}),
+	            drag: (this.data.drag!==0?this.data.drag:0),
+	            color: (this.data.color!==''?this.data.color:'#FFFFFF'),
+	            size: (this.data.size!==0?this.data.size:1),
+	            opacity: (this.data.opacity!==0?this.data.opacity:1),
+	            direction: (this.data.direction!==0?this.data.direction:1),
+	            duration: (this.data.duration!=null?this.data.duration:null),
+	            particleCount: (this.data.particleCount!==0?this.data.particleCount:200),
 	            texture: (this.data.texture!==''?this.data.texture:'./images/smokeparticle.png'),
 	            randomize: false
 	        };
