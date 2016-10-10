aa8a25f08f4f8025fd2c2d112002f8b8cb5cc23c
Chris Car <chris@metaversejs.org>
2016-10-10 10:47:02 -0400

    fixes #10

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ AFRAME.registerComponent('particle-system', {
         preset: {
             type: 'string',
             default: ''
-        }, 
+        },
         maxAge: {
             type: 'number'
         },
@@ -58,13 +58,13 @@ AFRAME.registerComponent('particle-system', {
         },
         particleCount: {
             type: 'number'
-        }, 
+        },
         texture: {
-            type: 'string'
+            type: 'src'
         },
         randomize: {
             type: 'boolean'
-        }, 
+        },
         opacity: {
           type: 'number',
         },
@@ -85,17 +85,17 @@ AFRAME.registerComponent('particle-system', {
             maxAge: (this.data.maxAge!==0?this.data.maxAge:6),
             positionSpread: (this.data.positionSpread.x!==0&&this.data.positionSpread.y!==0&&this.data.positionSpread.z!==0?this.data.positionSpread:{x:0,y:0,z:0}),
             type: (this.data.type!==0?this.data.type:SPE.distributions.BOX), /* SPE.distributions.SPHERE, SPE.distributions.DISC */
-            rotationAxis: (this.data.rotationAxis!==''?this.data.rotationAxis:'x'), 
-            rotationAngle: (this.data.rotationAngle!==0?this.data.rotationAngle:0), 
+            rotationAxis: (this.data.rotationAxis!==''?this.data.rotationAxis:'x'),
+            rotationAngle: (this.data.rotationAngle!==0?this.data.rotationAngle:0),
             accelerationValue: (this.data.accelerationValue.x!==0&&this.data.accelerationValue.y!==0&&this.data.accelerationValue.z!==0?this.data.accelerationValue:{x: 0, y: -10, z: 0}),
-            accelerationSpread: (this.data.accelerationSpread.x!==0&&this.data.accelerationSpread.y!==0&&this.data.accelerationSpread.z!==0?this.data.accelerationSpread:{x: 10, y: 0, z: 10}), 
+            accelerationSpread: (this.data.accelerationSpread.x!==0&&this.data.accelerationSpread.y!==0&&this.data.accelerationSpread.z!==0?this.data.accelerationSpread:{x: 10, y: 0, z: 10}),
             velocityValue: (this.data.velocityValue.x!==0&&this.data.velocityValue.y!==0&&this.data.velocityValue.z!==0?this.data.velocityValue:{x: 0, y: 25, z: 0}),
-            velocitySpread: (this.data.velocitySpread.x!==0&&this.data.velocitySpread.y!==0&&this.data.velocitySpread.z!==0?this.data.velocitySpread:{x: 10, y: 7.5, z: 10}), 
+            velocitySpread: (this.data.velocitySpread.x!==0&&this.data.velocitySpread.y!==0&&this.data.velocitySpread.z!==0?this.data.velocitySpread:{x: 10, y: 7.5, z: 10}),
             color: (this.data.color!==''?this.data.color:'#0000FF,#FF0000'),
             size: (this.data.size!==0?this.data.size:1),
             opacity: { value: (this.data.opacity!=0?this.data.opacity:1) },
             direction: (this.data.direction!==0?this.data.direction:1),
-            duration: (this.data.duration!=null?this.data.duration:null),
+            duration: (this.data.duration!=0?this.data.duration:null),
             particleCount: (this.data.particleCount!==0?this.data.particleCount:1000),
             texture: (this.data.texture!==''?this.data.texture:'https://cdn.rawgit.com/IdeaSpaceVR/aframe-particle-system-component/master/dist/images/star2.png'),
             randomize: false
@@ -106,17 +106,17 @@ AFRAME.registerComponent('particle-system', {
             maxAge: (this.data.maxAge!==0?this.data.maxAge:20),
             positionSpread: (this.data.positionSpread.x!==0&&this.data.positionSpread.y!==0&&this.data.positionSpread.z!==0?this.data.positionSpread:{x:100,y:100,z:100}),
             type: (this.data.type!==0?this.data.type:SPE.distributions.BOX), /* SPE.distributions.SPHERE, SPE.distributions.DISC */
-            rotationAxis: (this.data.rotationAxis!==''?this.data.rotationAxis:'x'), 
-            rotationAngle: (this.data.rotationAngle!==0?this.data.rotationAngle:3.14), 
+            rotationAxis: (this.data.rotationAxis!==''?this.data.rotationAxis:'x'),
+            rotationAngle: (this.data.rotationAngle!==0?this.data.rotationAngle:3.14),
             accelerationValue: (this.data.accelerationValue.x!==0&&this.data.accelerationValue.y!==0&&this.data.accelerationValue.z!==0?this.data.accelerationValue:{x: 0, y: 0, z: 0}),
-            accelerationSpread: (this.data.accelerationSpread.x!==0&&this.data.accelerationSpread.y!==0&&this.data.accelerationSpread.z!==0?this.data.accelerationSpread:{x: 0, y: 0, z: 0}), 
+            accelerationSpread: (this.data.accelerationSpread.x!==0&&this.data.accelerationSpread.y!==0&&this.data.accelerationSpread.z!==0?this.data.accelerationSpread:{x: 0, y: 0, z: 0}),
             velocityValue: (this.data.velocityValue.x!==0&&this.data.velocityValue.y!==0&&this.data.velocityValue.z!==0?this.data.velocityValue:{x: 1, y: 0.3, z: 1}),
-            velocitySpread: (this.data.velocitySpread.x!==0&&this.data.velocitySpread.y!==0&&this.data.velocitySpread.z!==0?this.data.velocitySpread:{x: 0.5, y: 1, z: 0.5}), 
+            velocitySpread: (this.data.velocitySpread.x!==0&&this.data.velocitySpread.y!==0&&this.data.velocitySpread.z!==0?this.data.velocitySpread:{x: 0.5, y: 1, z: 0.5}),
             color: (this.data.color!==''?this.data.color:'#FFFFFF'),
             size: (this.data.size!==0?this.data.size:1),
             opacity: { value: (this.data.opacity!=0?this.data.opacity:1) },
             direction: (this.data.direction!==0?this.data.direction:1),
-            duration: (this.data.duration!=null?this.data.duration:null),
+            duration: (this.data.duration!=0?this.data.duration:null),
             particleCount: (this.data.particleCount!==0?this.data.particleCount:100),
             texture: (this.data.texture!==''?this.data.texture:'https://cdn.rawgit.com/IdeaSpaceVR/aframe-particle-system-component/master/dist/images/smokeparticle.png'),
             randomize: false
@@ -127,17 +127,17 @@ AFRAME.registerComponent('particle-system', {
             maxAge: (this.data.maxAge!==0?this.data.maxAge:20),
             positionSpread: (this.data.positionSpread.x!==0&&this.data.positionSpread.y!==0&&this.data.positionSpread.z!==0?this.data.positionSpread:{x:100,y:100,z:100}),
             type: (this.data.type!==0?this.data.type:SPE.distributions.BOX), /* SPE.distributions.SPHERE, SPE.distributions.DISC */
-            rotationAxis: (this.data.rotationAxis!==''?this.data.rotationAxis:'x'), 
-            rotationAngle: (this.data.rotationAngle!==0?this.data.rotationAngle:3.14), 
+            rotationAxis: (this.data.rotationAxis!==''?this.data.rotationAxis:'x'),
+            rotationAngle: (this.data.rotationAngle!==0?this.data.rotationAngle:3.14),
             accelerationValue: (this.data.accelerationValue.x!==0&&this.data.accelerationValue.y!==0&&this.data.accelerationValue.z!==0?this.data.accelerationValue:{x: 0, y: 0, z: 0}),
-            accelerationSpread: (this.data.accelerationSpread.x!==0&&this.data.accelerationSpread.y!==0&&this.data.accelerationSpread.z!==0?this.data.accelerationSpread:{x: 0.2, y: 0, z: 0.2}), 
+            accelerationSpread: (this.data.accelerationSpread.x!==0&&this.data.accelerationSpread.y!==0&&this.data.accelerationSpread.z!==0?this.data.accelerationSpread:{x: 0.2, y: 0, z: 0.2}),
             velocityValue: (this.data.velocityValue.x!==0&&this.data.velocityValue.y!==0&&this.data.velocityValue.z!==0?this.data.velocityValue:{x: 0, y: 8, z: 0}),
-            velocitySpread: (this.data.velocitySpread.x!==0&&this.data.velocitySpread.y!==0&&this.data.velocitySpread.z!==0?this.data.velocitySpread:{x: 2, y: 0, z: 2}), 
+            velocitySpread: (this.data.velocitySpread.x!==0&&this.data.velocitySpread.y!==0&&this.data.velocitySpread.z!==0?this.data.velocitySpread:{x: 2, y: 0, z: 2}),
             color: (this.data.color!==''?this.data.color:'#FFFFFF'),
             size: (this.data.size!==0?this.data.size:1),
             opacity: { value: (this.data.opacity!=0?this.data.opacity:1) },
             direction: (this.data.direction!==0?this.data.direction:1),
-            duration: (this.data.duration!=null?this.data.duration:null),
+            duration: (this.data.duration!=0?this.data.duration:null),
             particleCount: (this.data.particleCount!==0?this.data.particleCount:200),
             texture: (this.data.texture!==''?this.data.texture:'https://cdn.rawgit.com/IdeaSpaceVR/aframe-particle-system-component/master/dist/images/smokeparticle.png'),
             randomize: false
@@ -148,17 +148,17 @@ AFRAME.registerComponent('particle-system', {
             maxAge: (this.data.maxAge!==0?this.data.maxAge:1),
             positionSpread: (this.data.positionSpread.x!==0&&this.data.positionSpread.y!==0&&this.data.positionSpread.z!==0?this.data.positionSpread:{x:100,y:100,z:100}),
             type: (this.data.type!==0?this.data.type:SPE.distributions.BOX), /* SPE.distributions.SPHERE, SPE.distributions.DISC */
-            rotationAxis: (this.data.rotationAxis!==''?this.data.rotationAxis:'x'), 
-            rotationAngle: (this.data.rotationAngle!==0?this.data.rotationAngle:3.14), 
+            rotationAxis: (this.data.rotationAxis!==''?this.data.rotationAxis:'x'),
+            rotationAngle: (this.data.rotationAngle!==0?this.data.rotationAngle:3.14),
             accelerationValue: (this.data.accelerationValue.x!==0&&this.data.accelerationValue.y!==0&&this.data.accelerationValue.z!==0?this.data.accelerationValue:{x: 0, y: 3, z: 0}),
-            accelerationSpread: (this.data.accelerationSpread.x!==0&&this.data.accelerationSpread.y!==0&&this.data.accelerationSpread.z!==0?this.data.accelerationSpread:{x: 2, y: 1, z: 2}), 
+            accelerationSpread: (this.data.accelerationSpread.x!==0&&this.data.accelerationSpread.y!==0&&this.data.accelerationSpread.z!==0?this.data.accelerationSpread:{x: 2, y: 1, z: 2}),
             velocityValue: (this.data.velocityValue.x!==0&&this.data.velocityValue.y!==0&&this.data.velocityValue.z!==0?this.data.velocityValue:{x: 0, y: 75, z: 0}),
-            velocitySpread: (this.data.velocitySpread.x!==0&&this.data.velocitySpread.y!==0&&this.data.velocitySpread.z!==0?this.data.velocitySpread:{x: 10, y: 50, z: 10}), 
+            velocitySpread: (this.data.velocitySpread.x!==0&&this.data.velocitySpread.y!==0&&this.data.velocitySpread.z!==0?this.data.velocitySpread:{x: 10, y: 50, z: 10}),
             color: (this.data.color!==''?this.data.color:'#FFFFFF'),
             size: (this.data.size!==0?this.data.size:0.4),
             opacity: { value: (this.data.opacity!=0?this.data.opacity:1) },
             direction: (this.data.direction!==0?this.data.direction:1),
-            duration: (this.data.duration!=null?this.data.duration:null),
+            duration: (this.data.duration!=0?this.data.duration:null),
             particleCount: (this.data.particleCount!==0?this.data.particleCount:1000),
             texture: (this.data.texture!==''?this.data.texture:'https://cdn.rawgit.com/IdeaSpaceVR/aframe-particle-system-component/master/dist/images/raindrop.png'),
             randomize: false
@@ -166,15 +166,20 @@ AFRAME.registerComponent('particle-system', {
 
 
     },
- 
+
 
     update: function (oldData) {
 
         this.clock = new THREE.Clock();
 
+        // Remove old particle group.
+        if (this.particleGroup) {
+            this.el.removeObject3D('particle-system');
+        }
+
         if (this.data.preset != '' && this.data.preset in this.presets) {
 
-            this.initParticleSystem(this.presets[this.data.preset]); 
+            this.initParticleSystem(this.presets[this.data.preset]);
 
         } else {
 
@@ -184,13 +189,19 @@ AFRAME.registerComponent('particle-system', {
     },
 
 
-    tick: function(time) {
+    tick: function(time, dt) {
 
+        //this.particleGroup.tick(dt);
         this.particleGroup.tick(this.clock.getDelta());
     },
 
 
-    remove: function() {},
+    remove: function() {
+
+        // Remove particle system.
+        if (!this.particleGroup) { return; }
+        this.el.removeObject3D('particle-system');
+    },
 
 
     initParticleSystem: function(settings) {
@@ -211,7 +222,7 @@ AFRAME.registerComponent('particle-system', {
 
         this.particleGroup = new SPE.Group({
             texture: {
-                value: particle_texture 
+                value: particle_texture
             },
             maxParticleCount: this.data.maxParticleCount
         });
@@ -230,14 +241,14 @@ AFRAME.registerComponent('particle-system', {
                 value: settings.type
             },
             position: {
-                value: this.el.object3D.position, 
+                value: this.el.object3D.position,
                 spread: new THREE.Vector3(settings.positionSpread.x, settings.positionSpread.y, settings.positionSpread.z),
                 randomize: settings.randomize
                 //spreadClamp: new THREE.Vector3( 2, 2, 2 ),
                 //radius: 4
             },
             rotation: {
-                axis: (settings.rotationAxis=='x'?new THREE.Vector3(1, 0, 0):(settings.rotationAxis=='y'?new THREE.Vector3(0, 1, 0):(settings.rotationAxis=='z'?new THREE.Vector3(0, 0, 1):new THREE.Vector3(0, 1, 0)))), 
+                axis: (settings.rotationAxis=='x'?new THREE.Vector3(1, 0, 0):(settings.rotationAxis=='y'?new THREE.Vector3(0, 1, 0):(settings.rotationAxis=='z'?new THREE.Vector3(0, 0, 1):new THREE.Vector3(0, 1, 0)))),
                 angle: settings.rotationAngle,
                 static: true
             },
@@ -246,11 +257,11 @@ AFRAME.registerComponent('particle-system', {
                 spread: new THREE.Vector3(settings.accelerationSpread.x, settings.accelerationSpread.y, settings.accelerationSpread.z)
             },
             velocity: {
-                value: new THREE.Vector3(settings.velocityValue.x, settings.velocityValue.y, settings.velocityValue.z), 
-                spread: new THREE.Vector3(settings.velocitySpread.x, settings.velocitySpread.y, settings.velocitySpread.z)  
+                value: new THREE.Vector3(settings.velocityValue.x, settings.velocityValue.y, settings.velocityValue.z),
+                spread: new THREE.Vector3(settings.velocitySpread.x, settings.velocitySpread.y, settings.velocitySpread.z)
             },
             color: {
-                value: color_arr 
+                value: color_arr
             },
             size: {
                 value: settings.size
@@ -262,15 +273,13 @@ AFRAME.registerComponent('particle-system', {
             direction: {
                 value: settings.direction
             },
-            duration: {
-                value: settings.duration
-            },
+            duration: settings.duration,
             opacity: settings.opacity,
             particleCount: settings.particleCount
         });
 
         this.particleGroup.addEmitter(emitter);
-        this.el.sceneEl.object3D.add(this.particleGroup.mesh);
+        this.el.setObject3D('particle-system', this.particleGroup.mesh);
     },
 
 
@@ -286,5 +295,3 @@ AFRAME.registerComponent('particle-system', {
     }
 
 });
-
-
